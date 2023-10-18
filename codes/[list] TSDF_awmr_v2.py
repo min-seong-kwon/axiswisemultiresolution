ef313bde5921d98514d61f8b4ca0b746b59c384a
Minseong <tiger11271@gmail.com>
2023-10-18 20:13:17 +0900

    thres list 수정, split + mesh + distance 측정까지

--- codes/[list] TSDF_awmr_v2.py
+++ codes/[list] TSDF_awmr_v2.py
@@ -12,6 +12,7 @@ from utils.evalUtils import key_is_in
 import trimesh
 import open3d as o3d
 from pandas import Series, DataFrame
+from source.vsMesh import vsMesh
 
 import igl
 import pickle
@@ -29,28 +30,26 @@ dataset_voxel_sizes = {
     'lucy': 1.0
 }
 # 데이터셋 선택
-dataset_name = 'asia'
+dataset_name = 'dragon'
 finest_voxel_size = dataset_voxel_sizes.get(dataset_name, None)
 scale_factor = 0.002/finest_voxel_size
+
 # GT 메시 로드
 gt_mesh_path = f'../OriginalDataset/{dataset_name}.ply'
-gt_mesh = trimesh.load(gt_mesh_path)
-# finest mesh 로드
-# finest_mesh_path = f'../0926_results/[TSDF]{dataset_name}/SingleRes/voxsize_{finest_voxel_size:.6f}/{dataset_name}_singleres=[32 32 32]_filled.ply'
-# finest_mesh = trimesh.load(finest_mesh_path)
+
+base_path = f'../1018_results/[TSDF]{dataset_name}/awmr/voxsize_{finest_voxel_size:.6f}/'
+subdirectories = ['pklfiles', 'original', 'filled']
+for subdirectory in subdirectories:
+    path = os.path.join(base_path, subdirectory)
+    os.makedirs(path, exist_ok=True)
+
+blockmesh_path = f'../_blockmeshes/{dataset_name}/' # for debug
+os.makedirs(blockmesh_path, exist_ok=True)
 volume_origin = np.load(f'../vunits/{dataset_name}/voxsize_{finest_voxel_size:.6f}/volume_origin_{finest_voxel_size:.6f}.npy')
-# 파일 저장 위치
-target_path = fr'../0926_results/[TSDF]{dataset_name}/awmr/voxsize_{finest_voxel_size:.6f}'
-blockmesh_path = f'../_meshes/{dataset_name}/axisres' # for debug
-if not os.path.exists(target_path):
-    os.makedirs(target_path, exist_ok=True)
-if not os.path.exists(blockmesh_path):
-    os.makedirs(blockmesh_path, exist_ok=True)
 
 ###############################################################################
 # 모든 해상도 데이터셋 로드
 ###############################################################################
-# dataset = {}
 volume_units = {}  
 
 res = np.array([8,16,32])
@@ -62,10 +61,7 @@ for axisres in tqdm(combinations):
     k: dataset과 volume unit index (ex) ('soldier',0,10,1)
     '''
     axisres_str = '_'.join(map(str, axisres))
-    # dataset[axisres_str] = MPEGDataset(dataset_name=dataset_name,
-    #                                     axisres=axisres,
-    #                                     finest_voxel_size=finest_voxel_size,
-    #                                     volume_origin=volume_origin)
+    
     dataset_axisres = MPEGDataset(dataset_name=dataset_name,
                                 axisres=axisres,
                                 finest_voxel_size=finest_voxel_size,
@@ -81,16 +77,23 @@ for axisres in tqdm(combinations):
 # 8x8x8 블록으로부터, 원하는 해상도까지 split
 # thres list 순회 (자동화)
 ###############################################################################
-thres_list = np.linspace(2e-9, 2e-6, 10) # np.logspace(0, 2.6, 10) * 5e-9
-filesize_list = [] 
+#thres_list = np.linspace(2e-9, 2e-6, 10) # np.logspace(0, 2.6, 10) * 5e-9
+thres_list = np.linspace(1.8e-9, 2.2e-6, 18) # 10.18 재실험 
+
+orifile_size_list = []
+filledfile_size_list = [] 
 num_blocks_list = []
-dist_original = []
-dist_finest = []
+chamferdist_list = []
+###############################################################################
 for thres in thres_list:
     thres2str = str(round(thres*1e6,3))
-    awmr_mesh_path = f'{target_path}/{dataset_name}_awmr_thres={thres2str}.ply'
-    awmr_tsdfs = {}
     
+    file_format = f'{dataset_name}_awmr_thres={thres2str}'
+    pklfile = os.path.join(base_path, 'pklfiles', f'{file_format}.pkl')
+    meshfile = os.path.join(base_path, 'original', f'{file_format}.ply')
+    filledfile = os.path.join(base_path, 'filled', f'{file_format}_filled.ply')
+    
+    awmr_tsdfs = {}
     for k in tqdm(volume_units['32_32_32'].keys(), desc=f"split awmr:{dataset_name}_{finest_voxel_size:.3f}, thres={thres2str}"):
         if len(k)==3:
             print("your initial key length is 3, please modify code")
@@ -116,8 +119,7 @@ for thres in thres_list:
         n_blocks += len(root.leaves)
     num_blocks_list.append(n_blocks)
     
-    pkl_path = awmr_mesh_path.replace('.ply', '.pkl')
-    with open(pkl_path, "wb") as f: 
+    with open(pklfile, "wb") as f: 
         pickle.dump(awmr_tsdfs, f)
     ###############################################################################
     # split된 TSDF block을 meshing
@@ -141,34 +143,30 @@ for thres in thres_list:
             continue
 
         mesh += block_mesh
-        # title = os.path.join(blockmesh_path,  f'{k[-3]}_{k[-2]}_{k[-1]}.ply')
-        # o3d.io.write_triangle_mesh(
-        #     title, block_mesh, write_ascii=True, write_vertex_colors=True)
-
-    o3d.io.write_triangle_mesh(awmr_mesh_path,
+    o3d.io.write_triangle_mesh(meshfile,
                                 mesh, write_ascii=True, write_vertex_colors=True)
-    del mesh
+    del mesh, awmr_tsdfs
     # mesh 후처리
-    filled_mesh_path = awmr_mesh_path.replace('.ply', '_filled.ply')
-    processed_mesh = trimesh.load(awmr_mesh_path)
+    processed_mesh = trimesh.load(meshfile)
     processed_mesh.update_faces(processed_mesh.nondegenerate_faces())
     trimesh.repair.fill_holes(processed_mesh)
-    processed_mesh.export(filled_mesh_path)
+    processed_mesh.export(filledfile)
+    del processed_mesh
     
-    # processed mesh 그리고 gt_mesh + finest_mesh의 거리를 측정 + append
-    file_size = os.path.getsize(filled_mesh_path) / 1024
-    # d_gt, d_finest = ChamferDistance_upscaled(processed_mesh, gt_mesh, finest_mesh)
-    # dist_original.append(d_gt)
-    # dist_finest.append(d_finest)
-    filesize_list.append(file_size)
+    orifile_size = os.path.getsize(meshfile) / 1024
+    filledfile_size = os.path.getsize(filledfile) / 1024
+    dist = vsMesh(gt_mesh_path, filledfile)
     
-    del awmr_tsdfs, processed_mesh
+    orifile_size_list.append(orifile_size)
+    filledfile_size_list.append(filledfile_size)
+    chamferdist_list.append(dist)
 
 raw_data = {'thres': thres_list,
-            'file size': filesize_list,
-            '# of blocks': num_blocks_list}
-            # 'original dist': dist_original,
-            # 'finest dist': dist_finest}
+            'ori file size': orifile_size_list,
+            'filled file size': orifile_size_list,
+            '# of blocks': num_blocks_list,
+            'chamfer distance': chamferdist_list}
+
 data = DataFrame(raw_data).transpose()
-data.to_excel(f'{target_path}/RD_{dataset_name}_awmr.xlsx', index=False)
 print(data)
+data.to_excel(f'../1018_results/[TSDF]{dataset_name}/awmr/voxsize_{finest_voxel_size:.6f}/RD_{dataset_name}_awmr.xlsx', index=True)
--- codes/[list] distance_measure_v3.py
+++ codes/[list] distance_measure_v3.py
@@ -9,7 +9,7 @@ import pandas
 from pandas import Series, DataFrame
 from tqdm import tqdm
 from source.vsMesh import vsMesh
-pandas.set_option('display.float_format', '{:.7f}'.format)
+pandas.set_option('display.float_format', '{:.9f}'.format)
 pandas.set_option('display.max_rows', None)
 ####################################################################################################
 dataset_voxel_sizes = {
@@ -20,17 +20,18 @@ dataset_voxel_sizes = {
     'happy': 0.0002,
     'lucy': 1.0
 }
-dataset_name = 'asia'
+dataset_name = 'dragon'
 finest_voxel_size = dataset_voxel_sizes.get(dataset_name, None)
-split = 'octree' # 'awmr' or 'octree' or 'SingleRes'
+split = 'SingleRes' # 'awmr' or 'octree' or 'SingleRes'
 target_obj_mesh_path = f'../OriginalDataset/{dataset_name}.ply'
 ###############################################################################
 dist_list = []
 th_list = []
 filesize_list = [] 
 singleres_list = [np.array([32,32,32]), np.array([16,16,16]), np.array([8,8,8])]
-thres_list = np.linspace(2e-9, 2e-6, 10) # for asia
+# thres_list = np.linspace(2e-9, 2e-6, 10) # for asia
 # thres_list = np.logspace(0, 1.6, 15) * 5e-8 # for others
+thres_list = np.linspace(1.8e-9, 2.2e-6, 18) # 10.18 재실험 
 thres_list = [str(round(th*1e6,3)) for th in thres_list]
 ###############################################################################
 print(f"[dataset: {dataset_name}] [split: {split}] [voxsize: {finest_voxel_size}]")
